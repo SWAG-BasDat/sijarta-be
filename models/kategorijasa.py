a7125306f3fc85c9cd5c4b42fd2130f76fc5b6cc
sopigoo <shofinadhifa24@gmail.com>
2024-12-05 10:48:03 +0700

    feat: add kategori jasa models

---
+++ models/kategorijasa.py
@@ -0,0 +1,15 @@
+class KategoriJasa:
+    def __init__(self, id, nama_kategori):
+        self.id = id
+        self.nama_kategori = nama_kategori
+
+    @staticmethod
+    def create_table(conn):
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS KATEGORI_JASA (
+                    Id UUID PRIMARY KEY,
+                    NamaKategori VARCHAR
+                );
+            """)
+            conn.commit()
