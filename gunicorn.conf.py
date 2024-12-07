601431f23298969dd75d5b5338ecfcdca17cad16
gracekarinn <gracekarina12@gmail.com>
2024-12-07 23:43:54 +0700

    feat: streamline Gunicorn configuration by setting a fixed bind address and adjusting logging settings

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,16 +1,11 @@
 import os
 
-port = os.getenv("PORT", "8080")
-bind = f"0.0.0.0:{port}"
-
-workers = 4  
-worker_class = 'gthread' 
+bind = "0.0.0.0:8080"
+workers = 4
+worker_class = 'gthread'
 threads = 2
 timeout = 120
 
 errorlog = '-'
 loglevel = 'debug'
 accesslog = '-'
-
-capture_output = True
-enable_stdio_inheritance = True
