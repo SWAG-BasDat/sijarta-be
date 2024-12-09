e8ba26dd8a2f46c494b220bdedeedb4f1fe3cec8
gracekarinn <gracekarina12@gmail.com>
2024-12-09 19:58:46 +0700

    refactor: clean up whitespace and correct column name in add_sesi_layanan method

--- services/sesilayanan_service.py
+++ services/sesilayanan_service.py
@@ -3,7 +3,7 @@ from models.sesilayanan import SesiLayanan
 class SesiLayananService:
     def __init__(self, conn):
         self.conn = conn
-
+        
     def get_sesi_by_subkategori(self, id_subkategori):
         with self.conn.cursor() as cur:
             cur.execute(
@@ -15,7 +15,7 @@ class SesiLayananService:
                 (str(id_subkategori),)
             )
             return cur.fetchall()
-        
+            
     def get_sesi_details(self, id_subkategori, sesi):
         with self.conn.cursor() as cur:
             cur.execute(
@@ -27,16 +27,16 @@ class SesiLayananService:
                 (str(id_subkategori), str(sesi))
             )
             return cur.fetchone()
-        
+            
     def add_sesi_layanan(self, id_subkategori, sesi, harga):
         with self.conn.cursor() as cur:
             try:
                 cur.execute(
                     """
-                    INSERT INTO SESI_LAYANAN (SubaktegoriId, Sesi, Harga)
+                    INSERT INTO SESI_LAYANAN (SubkategoriId, Sesi, Harga)
                     VALUES (%s, %s, %s)
                     """,
-                    (id_subkategori, sesi, harga)
+                    (str(id_subkategori), sesi, harga)
                 )
                 self.conn.commit()
             except Exception as e:
